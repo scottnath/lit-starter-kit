0f5179fe63e71e22ef70dcf08072a4b26bfc0fa4
Burton Smith <burton@breakstuff.io>
2024-12-01 21:15:18 -0500

    add framework type integrations

--- custom-elements-manifest.config.js
+++ custom-elements-manifest.config.js
@@ -2,6 +2,10 @@ import { getTsProgram, expandTypesPlugin } from 'cem-plugin-expanded-types';
 import { customElementReactWrapperPlugin } from 'custom-element-react-wrappers';
 import { customElementVsCodePlugin } from "custom-element-vs-code-integration";
 import { customElementJetBrainsPlugin } from "custom-element-jet-brains-integration";
+import { customElementSolidJsPlugin } from "custom-element-solidjs-integration";
+import { customElementJsxPlugin } from "custom-element-jsx-integration";
+import { customElementVuejsPlugin } from "custom-element-vuejs-integration";
+import { customElementSveltePlugin } from "custom-element-svelte-integration";
 
 export default {
   /** Globs to analyze */
@@ -17,6 +21,29 @@ export default {
     customElementJetBrainsPlugin(),
     customElementReactWrapperPlugin({
       outdir: 'react',
+      modulePath: (className, tagName) =>
+        `../dist/components/${tagName}/${className}.js`,
+    }),
+    customElementSolidJsPlugin({
+      outdir: 'types',
+      fileName: 'custom-element-solidjs.d.ts',
+      modulePath: (className, tagName) =>
+        `../dist/components/${tagName}/${className}.js`,
+    }),
+    customElementJsxPlugin({
+      outdir: 'types',
+      modulePath: (className, tagName) =>
+        `../dist/components/${tagName}/${className}.js`,
+    }),
+    customElementVuejsPlugin({
+      outdir: 'types',
+      fileName: 'custom-element-vuejs.d.ts',
+      modulePath: (className, tagName) =>
+        `../dist/components/${tagName}/${className}.js`,
+    }),
+    customElementSveltePlugin({
+      outdir: 'types',
+      fileName: 'custom-element-svelte.d.ts',
       modulePath: (className, tagName) =>
         `../dist/components/${tagName}/${className}.js`,
     }),
