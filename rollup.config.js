6c648e4fcb359a27fda9c468403b9cce851967e2
Burton Smith <burton@breakstuff.io>
2024-12-07 21:49:44 -0500

    remove extra tasks during watch mode

--- rollup.config.js
+++ rollup.config.js
@@ -11,6 +11,7 @@ import summary from 'rollup-plugin-summary';
 export default [
   /** bundle components for the CDN */
   {
+    watch: false,
     input: Object.fromEntries(
       globSync('src/**/*.ts', {
         ignore: [
@@ -51,9 +52,46 @@ export default [
       summary(),
     ],
   },
+  // watch version
+  {
+    input: Object.fromEntries(
+      globSync('src/**/*.ts', {
+        ignore: [
+          'src/**/*.test.ts',
+          'src/**/*.stories.ts',
+          'src/**/*.styles.ts',
+        ],
+      }).map(file => [
+        // This remove `src/` as well as the file extension from each
+        // file, so e.g. src/nested/foo.js becomes nested/foo
+        path.relative(
+          'src',
+          file.slice(0, file.length - path.extname(file).length),
+        ),
+        // This expands the relative paths to absolute paths, so e.g.
+        // src/nested/foo becomes /project/src/nested/foo.js
+        fileURLToPath(new URL(file, import.meta.url)),
+      ]),
+    ),
+    output: {
+      dir: 'cdn',
+      format: 'esm',
+    },
+    plugins: [
+      typescript({
+        tsconfig: 'tsconfig.json',
+        outDir: 'cdn',
+        sourceMap: false,
+        declaration: false,
+        declarationMap: false,
+      }),
+      resolve(),
+    ],
+  },
 
   /** bundle components for sandboxes */
   {
+    watch: false,
     input: globSync('src/**/index.ts'),
     output: {
       format: 'esm',
@@ -79,9 +117,31 @@ export default [
       summary(),
     ],
   },
+  // watch version
+  {
+    input: globSync('src/**/index.ts'),
+    output: {
+      format: 'esm',
+      dir: 'public/html',
+    },
+    plugins: [
+      typescript({
+        tsconfig: 'tsconfig.json',
+        outDir: 'public/html',
+        sourceMap: false,
+        declaration: false,
+        declarationMap: false,
+      }),
+      resolve(),
+      multi({
+        entryFileName: 'index.js',
+      }),
+    ],
+  },
 
   /** bundle react components for sandboxes */
   {
+    watch: false,
     input: 'react/index.js',
     output: {
       format: 'esm',
@@ -109,11 +169,42 @@ export default [
       console.error(warning.message);
     },
   },
+  // watch version
+  {
+    input: 'react/index.js',
+    output: {
+      format: 'esm',
+      file: 'public/react/index.js',
+      sourcemap: false,
+    },
+    external: ['react'],
+    plugins: [
+      resolve(),
+    ],
+    onwarn(warning) {
+      if (
+        /Could not resolve import/.test(warning.message) ||
+        /'this' keyword is equivalent to 'undefined'/.test(warning.message)
+      ) {
+        return;
+      }
+
+      console.error(warning.message);
+    },
+  },
   // bundle react component types for sandboxes
   {
+    watch: false,
     input: './react/index.d.ts',
     output: [{ file: 'public/react/index.d.ts', format: 'es' }],
     external: ['react'],
     plugins: [dts(), resolve(), summary()],
+  },
+  // watch version
+  {
+    input: './react/index.d.ts',
+    output: [{ file: 'public/react/index.d.ts', format: 'es' }],
+    external: ['react'],
+    plugins: [dts(), resolve()],
   },
 ];
